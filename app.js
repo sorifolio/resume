d1a71951b534770243a3c058fd30c1698e0687b3
coralcat <you@example.com>
2020-02-16 23:55:56 +0900

    레이아웃 간격, 폰트 조정

--- app.js
+++ app.js
@@ -4,33 +4,24 @@ const App = () => {
             <aside>
                 <section className="title">
                     <div className="picture"></div>
-                    <h1 className="name">김소리</h1>
-                    <p className="role">web developer</p>
-                </section>
-                <section className="profile">
-                    <p>
-
-                    </p>
+                    <h1 className="name kr">김소리</h1>
+                    <h4 className="role">web developer</h4>
                 </section>
                 <section className="information">
                     <ul>
                         <li>
-                            <small>name</small>
-                            <h5 className="kr">김소리</h5>
+                            <ion-icon name="mail"></ion-icon>
+                            <span>sori.developer@gmail.com</span>
                         </li>
                         <li>
-                            <small>role</small>
-                            <h5 className="kr">웹퍼블리셔, 웹디자이너</h5>
-                        </li>
-                        <li>
-                            <small>e-mail</small>
-                            <h5>sori.developer@gmail.com</h5>
-                        </li>
-                        <li>
-                            <small>phone</small>
-                            <h5>010 9690 5021</h5>
+                            <ion-icon name="call"></ion-icon>
+                            <span>010 9690 5021</span>
                         </li>
                     </ul>
+                </section>
+                <section className="profile kr">
+                    손이 빠른 편이며, 정보검색을 통하여 문제 해결이나 오픈소스를 유용하게 활용하는 편입니다.
+                    배움에 대한 열망이 강하여 여가시간을 허투루 보내지 않고, 끊임없이 공부하여 새로 나오는 기술들에 뒤쳐지지 않도록 노력하는 편입니다.
                 </section>
                 <section className="education">
                     <h3>education</h3>
@@ -46,7 +37,7 @@ const App = () => {
                     <ul>
                         <li><h4 className="kr">삼성 웰스토리</h4></li>
                         <li className="date">2019.08 - 2019.12</li>
-                        <li className="career"></li>
+                        <li className="career">삼성 웰스토리 모바일 오피스 UI 디자인 및 퍼블리싱</li>
                     </ul>
                 </section>
                 <section className="skills">
